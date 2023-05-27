b702835eee58bf9eb2e84f742a9d9dbd1e1a0073
chomapublicize <choma@publicize.co>
2023-05-27 11:22:52 -0500

    v1

---
+++ readme.md
@@ -0,0 +1,2 @@
+## generate tailwind css
+- npx tailwindcss -i ./tailwind.css -o ./assets/css/tailwindstyle.css --watch --minify
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   content: ["./**/*.html"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00030E",
+        secondary: "#010719",
+        tertiary: "#23243D",
+        quaternary: "#4E846E",
+        quinary: "#23243D",
+        sixth: "#676987",
+      },
+    },
+  },
   plugins: [],
 }
